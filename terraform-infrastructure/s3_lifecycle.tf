22516edbb6e4f8ac30008ff008c7f861a8702594
Olga Davladova <odavlad@gmail.com>
2026-02-12 20:30:58 +0100

    fix: add abort-mpu lifecycle + eventbridge notifications for log/replica buckets

--- terraform-infrastructure/s3_lifecycle.tf
+++ terraform-infrastructure/s3_lifecycle.tf
@@ -10,6 +10,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "app_bucket_lifecycle" {
     noncurrent_version_expiration {
       noncurrent_days = 30
     }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
 
